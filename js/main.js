f45519294064f58eb1da5734b2c5866dfc26c47c
owen <thss15_zhangqr@163.com>
2017-07-23 12:47:02 +0800

    restart

--- js/main.js
+++ js/main.js
@@ -1,21 +1,35 @@
- var canvas = document.getElementById("renderCanvas");
- var engine = new BABYLON.Engine(canvas, true);
 
- // This begins the creation of a function that we will 'call' just after it's built
- var createScene = function () 
- {
-    // Now create a basic Babylon Scene object 
+var canvas = document.getElementById("renderCanvas");
+var engine = new BABYLON.Engine(canvas, true);
+canvas.width = window.width;
+canvas.height = window.height;
+var createScene = function () 
+{
+    /*
+        var scene = new BABYLON.Scene(engine);
+        scene.clearColor = new BABYLON.Color3.White();
+        //var camera = new BABYLON.FreeCamera("camera1", new BABYLON.Vector3(0, 0, 5), scene);
+        var camera = new BABYLON.ArcRotateCamera("arcCamera",BABYLON.Tools.ToRadians(45),BABYLON.Tools.ToRadians(45),10.0,box.position,scene);
+        camera.setTarget(BABYLON.Vector3.Zero());  
+        var box = BABYLON.Mesh.CreateBox("name",4.0,scene);
+        var box2 = BABYLON.Mesh.CreateBox("name",4.0,scene);
+        return scene;
+    */
     var scene = new BABYLON.Scene(engine);
-
-    // Change the scene background color to green.
-    scene.clearColor = new BABYLON.Color3(0, 1, 0);
-
-    // This creates and positions a free camera
-    var camera = new BABYLON.FreeCamera("camera1", new BABYLON.Vector3(0, 5, -10), scene);
-
-    
+    var light = new BABYLON.PointLight("Omni", new BABYLON.Vector3(0,100,100),scene);
+    var camera = new BABYLON.ArcRotateCamera("camera",0,0.8,100,BABYLON.Vector3.Zero(),scene);
+    var sphere1 = new BABYLON.Mesh.CreateSphere("Sphere1",10.0,6.0,scene);
+    sphere1.position.x = -40;
+    return scene;
+}
+var scene = createScene();
+//循环
+engine.runRenderLoop(function () {
+    scene.render();
+  });
+/*
     // This targets the camera to scene origin
-    camera.setTarget(BABYLON.Vector3.Zero());
+                       
 
     // This attaches the camera to the canvas
     camera.attachControl(canvas, false);
@@ -39,10 +53,7 @@
     return scene;
 
   };  // End of createScene function
-var scene = createScene();
-engine.runRenderLoop(function () {
-    scene.render();
-  });
+
 window.addEventListener("resize", function () {
     engine.resize();
-  });
+  });*/
